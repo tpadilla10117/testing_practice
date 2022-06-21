2d27af228ff09f4ac328fce06830117795dc10f7
Trinidad Padilla <tpadilla101@live.com>
2022-06-21 14:31:01 -0700

    set up ytesting

---
+++ src/Script.js
@@ -0,0 +1,15 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavoritecats.com'
+];
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    });
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+};

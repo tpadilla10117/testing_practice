80cc892d20d7973752d1ba72bc36aa47f79247a2
Trinidad Padilla <tpadilla101@live.com>
2022-06-22 11:16:56 -0700

    added some tests

---
+++ src/AsyncScript.test.js
@@ -0,0 +1,1 @@
+/* Testing Asynchrous Tests in Jest: */
--- src/Script.test.js
+++ src/Script.test.js
@@ -9,11 +9,24 @@
     ];
 
 /* This is a sample of assertion library: */
-    it('is a sample test', () => {
-        expect('hello').toBe('hello')
-    })
 
-    it('is searching google', () => {
-        expect(googleSearch('testtest', dbMock) ).toEqual([])
-        expect(googleSearch('dog', dbMock) ).toEqual(['dog.com', 'dogpictures.com'])
+    describe('googleSearch', () => {
+
+        it('is a sample test', () => {
+            expect('hello').toBe('hello')
+        })
+
+        it('is searching google', () => {
+            expect(googleSearch('testtest', dbMock) ).toEqual([])
+            expect(googleSearch('dog', dbMock) ).toEqual(['dog.com', 'dogpictures.com'])
+        })
+
+        it('work with undefined and null input', () => {
+            expect(googleSearch(undefined, dbMock)).toEqual([]);
+            expect(googleSearch(null, dbMock)).toEqual([]);
+        })
+
+        it('Does not return more than 3 matches', () => {
+            expect(googleSearch('.com', dbMock).length).toEqual(3);
+        })
     })
